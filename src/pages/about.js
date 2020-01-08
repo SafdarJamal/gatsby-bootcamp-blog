6cd4de038b8642ef02366a9225da60d464d3972a
Safdar Jamal <safdar.dev@gmail.com>
2020-01-08 23:26:27 +0500

    Add contact link in about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,7 +1,16 @@
 import React from 'react';
+import { Link } from 'gatsby';
 
 const About = () => {
-  return <div>About</div>;
+  return (
+    <div>
+      <h1>About</h1>
+      <p>I'm a Software Developer</p>
+      <p>
+        <Link to="/contact">Contact me</Link>
+      </p>
+    </div>
+  );
 };
 
 export default About;
