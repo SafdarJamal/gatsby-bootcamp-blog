0d43ce5989eadc40e245cd1f1c81e389af700c8e
Safdar Jamal <safdar.dev@gmail.com>
2020-01-09 00:27:32 +0500

    Create universal layout component

---
+++ src/components/layout.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Header from './header';
+import Footer from './footer';
+
+const Layout = ({ children }) => {
+  return (
+    <div>
+      <Header />
+      {children}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
