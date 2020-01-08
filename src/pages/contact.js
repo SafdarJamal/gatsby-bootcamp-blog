e6857632e399a24bc14ea241dc15763b023bc4ff
Safdar Jamal <safdar.dev@gmail.com>
2020-01-09 01:14:48 +0500

    Add rel attribute in anchor tag for security

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -8,7 +8,11 @@ const Contact = () => {
       <h1>Contact</h1>
       <p>
         The best way to reach me is via{' '}
-        <a href="https://twitter.com/_safdarjamal" target="_blank">
+        <a
+          href="https://twitter.com/_safdarjamal"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           @_safdarjamal
         </a>{' '}
         on Twitter.
