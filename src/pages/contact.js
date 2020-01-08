fa1df306fd86dd0fefcc1a64c47fac659d6c325c
Safdar Jamal <safdar.dev@gmail.com>
2020-01-08 23:34:47 +0500

    Add external link in contact page

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,7 +1,18 @@
 import React from 'react';
 
 const Contact = () => {
-  return <div>Contact</div>;
+  return (
+    <div>
+      <h1>Contact</h1>
+      <p>
+        The best way to reach me is via{' '}
+        <a href="https://twitter.com/_safdarjamal" target="_blank">
+          @_safdarjamal
+        </a>{' '}
+        on Twitter.
+      </p>
+    </div>
+  );
 };
 
 export default Contact;
