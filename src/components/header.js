10088e0e6786fe3c2e8e22f76b53f3e52018fdbf
Safdar Jamal <safdar.dev@gmail.com>
2020-01-09 19:56:33 +0500

    Style header component with css module

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,53 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import styles from './header.module.scss';
+
 const Header = () => {
   return (
-    <header>
-      <h1>GBB</h1>
+    <header className={styles.header}>
+      <h1>
+        <Link className={styles.title} to="/">
+          Safdar Jamal
+        </Link>
+      </h1>
       <nav>
-        <ul>
+        <ul className={styles.navList}>
           <li>
-            <Link to="/">Home</Link>
+            <Link
+              className={styles.navItem}
+              activeClassName={styles.activeNavItem}
+              to="/"
+            >
+              Home
+            </Link>
           </li>
           <li>
-            <Link to="/about">About</Link>
+            <Link
+              className={styles.navItem}
+              activeClassName={styles.activeNavItem}
+              to="/about"
+            >
+              About
+            </Link>
           </li>
           <li>
-            <Link to="/blog">Blog</Link>
+            <Link
+              className={styles.navItem}
+              activeClassName={styles.activeNavItem}
+              to="/blog"
+            >
+              Blog
+            </Link>
           </li>
           <li>
-            <Link to="/contact">Contact</Link>
+            <Link
+              className={styles.navItem}
+              activeClassName={styles.activeNavItem}
+              to="/contact"
+            >
+              Contact
+            </Link>
           </li>
         </ul>
       </nav>
