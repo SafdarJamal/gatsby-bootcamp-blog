c9ce7cd0d3522d601bed49257baf8d956e297c9f
Safdar Jamal <safdar.dev@gmail.com>
2020-01-10 04:46:00 +0500

    Dynamic pages from contentful

--- src/templates/blog.js
+++ src/templates/blog.js
@@ -1,28 +1,40 @@
 import React from 'react';
 import { graphql } from 'gatsby';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
 
 import Layout from '../components/layout';
 
 export const query = graphql`
   query($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
-      frontmatter {
-        title
-        date
+    contentfulBlogPost(slug: { eq: $slug }) {
+      title
+      date(formatString: "MMMM Do, YYYY")
+      body {
+        json
       }
-      html
     }
   }
 `;
 
 const Blog = props => {
+  const options = {
+    renderNode: {
+      'embedded-asset-block': node => {
+        const src = node.data.target.fields.file['en-US'].url;
+        const alt = node.data.target.fields.title['en-US'];
+        return <img src={src} alt={alt} />;
+      },
+    },
+  };
+
   return (
     <Layout>
-      <h1>{props.data.markdownRemark.frontmatter.title}</h1>
-      <p>{props.data.markdownRemark.frontmatter.date}</p>
-      <div
-        dangerouslySetInnerHTML={{ __html: props.data.markdownRemark.html }}
-      ></div>
+      <h1>{props.data.contentfulBlogPost.title}</h1>
+      <p>{props.data.contentfulBlogPost.date}</p>
+      {documentToReactComponents(
+        props.data.contentfulBlogPost.body.json,
+        options
+      )}
     </Layout>
   );
 };
