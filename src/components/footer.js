147304b7a418ec8c4eaa41a002cd5ab7714455e6
Safdar Jamal <safdar.dev@gmail.com>
2020-01-10 01:23:36 +0500

    Style blog page

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { graphql, useStaticQuery } from 'gatsby';
 
+import styles from './footer.module.scss';
+
 const Footer = () => {
   const data = useStaticQuery(graphql`
     query {
@@ -13,7 +15,7 @@ const Footer = () => {
   `);
 
   return (
-    <footer>
+    <footer className={styles.footer}>
       <p>Created by {data.site.siteMetadata.author} © 2020</p>
     </footer>
   );
--- src/pages/blog.js
+++ src/pages/blog.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { graphql, useStaticQuery, Link } from 'gatsby';
 
 import Layout from '../components/layout';
+import styles from './blog.module.scss';
 
 const Blog = () => {
   const data = useStaticQuery(graphql`
@@ -25,10 +26,10 @@ const Blog = () => {
   return (
     <Layout>
       <h1>Blog</h1>
-      <ol>
+      <ol className={styles.posts}>
         {data.allMarkdownRemark.edges.map((edge, i) => {
           return (
-            <li key={i}>
+            <li className={styles.post} key={i}>
               <Link to={`/blog/${edge.node.fields.slug}`}>
                 <h2>{edge.node.frontmatter.title}</h2>
                 <p>{edge.node.frontmatter.date}</p>
