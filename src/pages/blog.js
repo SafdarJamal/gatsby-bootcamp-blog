66ea71e8424e4e89eab5b6742ca5880dedc3d82e
Safdar Jamal <safdar.dev@gmail.com>
2020-01-09 22:29:13 +0500

    Query all markdown posts and render it in a list.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,12 +1,42 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
 import Layout from '../components/layout';
 
 const Blog = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      allMarkdownRemark {
+        edges {
+          node {
+            html
+            headings {
+              depth
+              value
+            }
+            frontmatter {
+              title
+              date
+            }
+          }
+        }
+      }
+    }
+  `);
+
   return (
     <Layout>
       <h1>Blog</h1>
-      <p>Posts will show up here later on.</p>
+      <ol>
+        {data.allMarkdownRemark.edges.map((edge, i) => {
+          return (
+            <li key={i}>
+              <h2>{edge.node.frontmatter.title}</h2>
+              <p>{edge.node.frontmatter.date}</p>
+            </li>
+          );
+        })}
+      </ol>
     </Layout>
   );
 };
