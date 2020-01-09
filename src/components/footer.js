1eb99c43e643d1bd629953cd9084aa17d5f3ab6c
Safdar Jamal <safdar.dev@gmail.com>
2020-01-09 20:31:25 +0500

    Get footer data dynamically with GraphQL

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,9 +1,20 @@
 import React from 'react';
+import { graphql, useStaticQuery } from 'gatsby';
 
 const Footer = () => {
+  const data = useStaticQuery(graphql`
+    query {
+      site {
+        siteMetadata {
+          author
+        }
+      }
+    }
+  `);
+
   return (
     <footer>
-      <p>Created by Safdar Jamal © 2020</p>
+      <p>Created by {data.site.siteMetadata.author} © 2020</p>
     </footer>
   );
 };
