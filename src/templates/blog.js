a7e0266e14054b16d865f54d9a36cd335394f51e
Safdar Jamal <safdar.dev@gmail.com>
2020-01-09 23:45:07 +0500

    Create blog template

---
+++ src/templates/blog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Layout from '../components/layout';
+
+const Blog = () => {
+  return (
+    <Layout>
+      <p>This is the blog template.</p>
+    </Layout>
+  );
+};
+
+export default Blog;
